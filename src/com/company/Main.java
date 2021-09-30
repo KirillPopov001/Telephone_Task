16c3c13f2841aac101a7bf953c4be8164bf97d4d
Kirill Popov <kirya_popov2000@mail.ru>
2021-09-30 10:45:54 +0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws RecordNotFound, RecordNotValid, PhoneNumberAlreadyExists {
+	Record phone1 = new Record(1000001, "89619997418", "Kirill");
+    Record phone2 = new Record(1000002, "89609547080", "Diana");
+    Record phone3 = new Record(1000003, "89619997418", "Hohol");
+    Record phone4 = new Record(1000004, "89139065473", "Hohol2");
+
+    PhoneBook book = new PhoneBook();
+    try {
+        book.createRecord(phone1);
+        book.createRecord(phone2);
+        book.createRecord(phone3);
+        book.createRecord(phone4);
+    }catch(PhoneNumberAlreadyExists e){
+        System.out.println(e.getMessage());
+        throw e;
+    }
+    System.out.println(book.getAllRecords());
+    try {
+        book.updateRecord(phone1);
+    }catch(RecordNotValid e){
+        System.out.println(e.getMessage());
+        throw e;
+        }
+    System.out.println(book.getAllRecords());
+    book.deleteRecord(1000008);
+    System.out.println(book.getAllRecords());
+    }
+}
---
+++ src/com/company/RecordNotFound.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class RecordNotFound extends RuntimeException {
+
+    public RecordNotFound(String message){
+
+        super(message);
+    }
+}
